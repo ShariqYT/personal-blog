64e70530f4e53dc8a6e7f7a8ada137d0da832146
Mohd Shariq <111305981+ShariqYT@users.noreply.github.com>
2022-08-22 11:07:35 +0530

    new version

--- js/resp.js
+++ js/resp.js
@@ -6,6 +6,6 @@ navRight = document.querySelector('.nav-right')
 
 burger.addEventListener('click', () => {
     navList.classList.toggle('v-class-resp');
-    navRight.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
+    navRight.classList.toggle('v-class-resp');
 })
